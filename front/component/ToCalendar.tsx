8a6b92e883d501531cee41ec822b8ad1a0edda4b
cou <courange.c@gmail.com>
2022-12-19 07:48:20 +0900

    カレンダーリストを同期的に取得できない

--- front/component/ToCalendar.tsx
+++ front/component/ToCalendar.tsx
@@ -6,7 +6,8 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import {useState} from "react";
 
-export default async function ToCalendar({apiCalendar}: {apiCalendar: ApiCalendar}) {
+export default function ToCalendar({apiCalendar}: {apiCalendar: ApiCalendar}) {
+    let [res, setRes] = useState(null);
     let [isSignedIn, setIsSignedIn] = useState(apiCalendar.sign);
     apiCalendar.onLoadCallback = () => {
         apiCalendar.tokenClient.callback = (isSigned: boolean) => {
@@ -14,17 +15,22 @@ export default async function ToCalendar({apiCalendar}: {apiCalendar: ApiCalenda
         };
     };
     if (!isSignedIn) return <SigninBtns apiCalendar={apiCalendar}></SigninBtns>;
-    let res = await apiCalendar.listCalendars();
-    console.log(res);
+    setRes(apiCalendar.listCalendars()).then((res) => {
+        console.log(res);
+        setIsLoading(false);
+        return (
+            <Select>
+                {res.result.items.map((calendar: any) => {
+                    return (
+                        <MenuItem value={calendar.id} key={calendar.id}>
+                            {calendar.summary}
+                        </MenuItem>
+                    );
+                })}
+            </Select>
+        );
+    });
     return (
-        <Select>
-            {res.result.items.map((calendar: any) => {
-                return (
-                    <MenuItem value={calendar.id} key={calendar.id}>
-                        {calendar.summary}
-                    </MenuItem>
-                );
-            })}
-        </Select>
-    );
+        <p>dummy</p>
+    )
 }

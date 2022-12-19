84a0c07e2798e8f9731700768c9ba7d84318e5db
cou <courange.c@gmail.com>
2022-12-19 22:07:49 +0900

    add chat gpt answer

--- front/component/ToCalendar.tsx
+++ front/component/ToCalendar.tsx
@@ -1,49 +1,50 @@
-import FormControl from "@mui/material/FormControl";
-import FormLabel from "@mui/material/FormLabel";
-import ApiCalendar from "react-google-calendar-api";
-import SigninBtns from "./SigninBtns";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
 import {useState, useEffect} from "react";
+const {google} = require("googleapis");
+const calendar = google.calendar("v3");
 
-export default function ToCalendar({apiCalendar}: {apiCalendar: ApiCalendar}) {
-    let [calendarList, setCalendarList] = useState<any>();
-    let [isSignedIn, setIsSignedIn] = useState(apiCalendar.sign);
+import React from "react";
 
-    apiCalendar.onLoadCallback = () => {
-        console.log("loading");
-        apiCalendar.tokenClient.callback = (isSigned: boolean) => {
-            setIsSignedIn(isSigned);
-        };
-        console.log("set use effect");
-        useEffect(() => {
-            async function fetchData() {
-                console.log("fetch");
-                let response = await apiCalendar.listCalendars();
-                setCalendarList(response.result.item);
-            }
-            fetchData();
-        }, []);
-    };
+const CLIENT_ID = "822488502520-u0cv3d11emdejo5o4duom4etrr3a9f0s.apps.googleusercontent.com",;
+const API_KEY = "AIzaSyC_fWzofzRcMFwZZ4YUJmF1g8Hrc8m-rO8";
+const DISCOVERY_DOC = "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
+const SCOPES = "https://www.googleapis.com/auth/calendar";
 
-    if (!isSignedIn) return <SigninBtns apiCalendar={apiCalendar}></SigninBtns>;
-    else
-        return (
-            <FormControl>
-                <FormLabel>読み込むカレンダー</FormLabel>
-                <Select>
-                    {calendarList ? (
-                        calendarList.map((calendar: any) => {
-                            return (
-                                <MenuItem value={calendar.id} key={calendar.id}>
-                                    {calendar.summary}
-                                </MenuItem>
-                            );
-                        })
-                    ) : (
-                        <p>is loading</p>
-                    )}
-                </Select>
-            </FormControl>
-        );
-}
+const config = {
+    clientId: "822488502520-u0cv3d11emdejo5o4duom4etrr3a9f0s.apps.googleusercontent.com",
+    apiKey: "AIzaSyC_fWzofzRcMFwZZ4YUJmF1g8Hrc8m-rO8",
+    scope: "https://www.googleapis.com/auth/calendar",
+};
+
+const ToCalendar = () => {
+    const [calendars, setCalendars] = useState([]);
+
+    useEffect(() => {
+        async function fetchCalendars() {
+            const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, YOUR_CLIENT_SECRET, YOUR_REDIRECT_URL);
+
+            // Set the correct scopes
+            oAuth2Client.setCredentials({
+                refresh_token: YOUR_REFRESH_TOKEN,
+            });
+
+            const calendarList = await calendar.calendarList.list({auth});
+            setCalendars(calendarList.data.items);
+        }
+
+        fetchCalendars();
+    }, []);
+
+    return (
+        <div>
+            <h1>Calendar List</h1>
+            {calendars.map((calendar) => (
+                <div key={calendar.id}>
+                    <h2>{calendar.summary}</h2>
+                    <p>{calendar.description}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ToCalendar;
--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -10,26 +10,16 @@ import ApiCalendar from "react-google-calendar-api";
 
 const BaseUrl = "https://www.googleapis.com/calendar/v3";
 
-const CLIENT_ID = "822488502520-6hbn6f2emi5kgnqqj0acesk396gmuhak.apps.googleusercontent.com";
-const API_KEY = "AIzaSyC_fWzofzRcMFwZZ4YUJmF1g8Hrc8m-rO8";
-const DISCOVERY_DOC = "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest";
-const SCOPES = "https://www.googleapis.com/auth/calendar";
-
-const config = {
-    clientId: "822488502520-u0cv3d11emdejo5o4duom4etrr3a9f0s.apps.googleusercontent.com",
-    apiKey: "AIzaSyC_fWzofzRcMFwZZ4YUJmF1g8Hrc8m-rO8",
-    scope: "https://www.googleapis.com/auth/calendar",
-    discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+scoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
 };
 
 export default function Home() {
-    const apiCalendar = new ApiCalendar(config);
     return (
         <div>
             <Header />
             <Container>
                 <ImportRange />
-                <ToCalendar apiCalendar={apiCalendar} />
+                <ToCalendar/>
                 <DHUPortalData />
                 <ImportOptions />
             </Container>

c4aca7c67b0b256727871979d7ab6c08658175b2
cou <courange.c@gmail.com>
2022-12-20 22:38:27 +0900

    add name to form children

--- front/components/import_module/ImportRange.tsx
+++ front/components/import_module/ImportRange.tsx
@@ -5,7 +5,7 @@ export default function Component() {
 
             <FormControl fullWidth margin="dense">
                 <InputLabel id="import-q-label">インポートするクオーター</InputLabel>
-                <Select labelId="import-q-label" label="インポートするクオーター">
+                <Select name="import_quarter" labelId="import-q-label" label="インポートするクオーター">
                     <MenuItem value="1">1</MenuItem>
                     <MenuItem value="2">2</MenuItem>
                     <MenuItem value="3">3</MenuItem>

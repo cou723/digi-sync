5892ed69cd6d4fe4facb75934740e5440ed0ff47
cou <courange.c@gmail.com>
2022-12-25 10:26:38 +0900

    fix build error

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -42,7 +42,6 @@ export default function Home() {
     let [totalImportCount, setTotalImportCount] = useState<number>(0);
     let [isDHUPortalWaiting, setIsDHUPortalWaiting] = useState<boolean>(false);
     let [isGoogleLogin, setIsGoogleLogin] = useState<boolean>(false);
-    let  =useSession()
 
     const importCountIncrement = () => setImportCount((prevCount) => prevCount + 1);
 

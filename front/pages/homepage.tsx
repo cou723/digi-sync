900fd76e6b7cbfca4da45bd23994d8c34e422026
cou <courange.c@gmail.com>
2022-12-26 11:40:16 +0900

    add app description

---
+++ front/pages/homepage.tsx
@@ -0,0 +1,10 @@
+import {Typography} from '@mui/material';
+
+export default function homepage(){
+    return(
+        <>
+            <Typography>このアプリケーションについて</Typography>
+            <Typography></Typography>
+        </>
+    );
+}

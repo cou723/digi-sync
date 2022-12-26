01662e67d3fe8308f705156349426af15041f058
cou <courange.c@gmail.com>
2022-12-26 11:45:51 +0900

    upgrade ui

--- front/pages/terms_and_conditions.tsx
+++ front/pages/terms_and_conditions.tsx
@@ -4,7 +4,7 @@ export default function privacy_policy() {
     return (
         <>
             <Container maxWidth="md">
-                <Typography variant="h2" gutterBottom>
+                <Typography variant="h4" component="h1" gutterBottom>
                     利用規約
                 </Typography>
                 <Typography variant="body1">

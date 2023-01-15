29af300fcfe6719ff353da28355b5f706a935a05
cou <courange.c@gmail.com>
2023-01-15 21:48:31 +0900

    fix about

--- front/pages/about.tsx
+++ front/pages/about.tsx
@@ -7,7 +7,16 @@ export default function about() {
                 <Typography variant="h4" component="h1">
                     このアプリケーションについて
                 </Typography>
-                <Typography variant="body1">このアプリケーションは、デジタルキャンパスから取得したイベントをGoogleカレンダーにインポートするためのものです。</Typography>
+                <Typography variant="body1">このアプリケーションは、デジキャンから取得したイベントをGoogleカレンダーに追加します。</Typography>
+                <Typography variant="h4" component="h2">
+                    Googleでログインした際のユーザーデータを何に扱うか
+                </Typography>
+                <Typography variant="body1">
+                    このアプリケーションは、まず、Googleに関する情報は一切保存しません。
+                    <br />
+                    また、メールアドレスは今現在あなたがどのアカウントでログインしているかを示すために表示されているだけで、そのほかの目的では一切利用していません。
+                </Typography>
+
             </Container>
         </>
     );

0fd681fd0f91cfc0ba30e98d167890cc5429eaaa
cou <courange.c@gmail.com>
2023-01-30 12:34:10 +0900

    add debug info

--- front/pages/privacy_policy.tsx
+++ front/pages/privacy_policy.tsx
@@ -11,6 +11,7 @@ export default function privacy_policy() {
                     第1条 （個人情報取り扱いに関する基本方針）
                 </Typography>
                 <Typography variant="body1">当サイトではログインにGoogleを利用しておりますが、それによりいかなる個人情報を収集することはありません。また、第三者に提供することもありません。</Typography>
+                <Typography variant="body1">また、メールアドレスは今現在あなたがどのアカウントでログインしているかを示すために表示されているだけで、そのほかの目的では一切利用していません。</Typography>
                 <Typography variant="h5" component="h2" gutterBottom>
                     第2条（プライバシーポリシーの変更）
                 </Typography>

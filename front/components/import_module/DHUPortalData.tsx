e900e63dc6a777d703020d14f4188c8efd7c3262
cou <courange.c@gmail.com>
2023-02-10 22:37:51 +0900

    バリデーション追加

--- front/components/import_module/DHUPortalData.tsx
+++ front/components/import_module/DHUPortalData.tsx
@@ -1,17 +1,25 @@
-import {FormControl, TextField, Stack} from "@mui/material";
+import {FormControl, TextField, Stack, FormHelperText} from "@mui/material";
 import {ChangeEvent} from "react";
 
 type Props = {
+    error: Error;
     username: string;
     password: string;
     onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 };
 
-export default function DHUPortalData({username, password, onChange}: Props) {
+type Error = {
+    username: string;
+    password: string;
+};
+
+export default function DHUPortalData({error, username, password, onChange}: Props) {
     return (
         <Stack spacing={1}>
-            <TextField onChange={onChange} value={username} required name="username" id="standard-basic" label="デジキャン ユーザーネーム" variant="standard" />
-            <TextField type="password" onChange={onChange} value={password} required name="password" id="standard-basic" label="デジキャン パスワード" variant="standard" />
+            <TextField error={error.username != ""} onChange={onChange} value={username} required name="username" id="standard-basic" label="デジキャン ユーザーネーム" variant="standard" />
+            <FormHelperText>{error.username}</FormHelperText>
+            <TextField error={error.password != ""} type="password" onChange={onChange} value={password} required name="password" id="standard-basic" label="デジキャン パスワード" variant="standard" />
+            <FormHelperText>{error.password}</FormHelperText>
         </Stack>
     );
 }
--- front/components/import_module/ImportRange.tsx
+++ front/components/import_module/ImportRange.tsx
@@ -1,17 +1,17 @@
-import {useSession, signIn, signOut} from "next-auth/react";
 import {ReactNode} from "react";
-import {FormControl, Select, MenuItem, InputLabel, SelectChangeEvent} from "@mui/material";
+import {FormControl, Select, MenuItem, InputLabel, SelectChangeEvent, FormHelperText} from "@mui/material";
 
 type Props = {
+    error: string;
     value: string;
     onChange: (event: SelectChangeEvent<string>, child: ReactNode) => void;
 };
 
-export default function Component({value, onChange}: Props) {
+export default function Component({error, value, onChange}: Props) {
     return (
         <FormControl fullWidth margin="normal">
             <InputLabel id="import-q-label">インポートするクオーター</InputLabel>
-            <Select onChange={onChange} value={value} required name="importRange" labelId="import-q-label" label="インポートするクオーター">
+            <Select error={error != ""} onChange={onChange} value={value} required name="importRange" labelId="import-q-label" label="インポートするクオーター">
                 <MenuItem value="1q">1クオーター</MenuItem>
                 <MenuItem value="2q">2クオーター</MenuItem>
                 <MenuItem value="3q">3クオーター</MenuItem>
@@ -19,6 +19,7 @@ export default function Component({value, onChange}: Props) {
                 <MenuItem value="1q_and_2q">前期</MenuItem>
                 <MenuItem value="3q_and_4q">後期</MenuItem>
             </Select>
+            <FormHelperText>{error}</FormHelperText>
         </FormControl>
     );
 }

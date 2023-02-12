51cccebc18e6f7d1125094b4c112b0525f066360
cou <courange.c@gmail.com>
2023-02-12 23:16:55 +0900

    重複を追加しないよう修正

--- front/components/ImportForm.tsx
+++ front/components/ImportForm.tsx
@@ -5,6 +5,8 @@ import ToCalendar from "./import_module/ToCalendar";
 import {Button, Stack, SelectChangeEvent, Select, InputLabel, FormControl, MenuItem} from "@mui/material";
 import {useState, useEffect, ChangeEvent, ReactNode} from "react";
 import {useSession} from "next-auth/react";
+import type {CalendarList, Event} from "../gapi_calendar.d.ts";
+import {start} from "repl";
 
 type Inputs = {
     importYear: string;
@@ -33,7 +35,29 @@ function get_end_time(start: string): string {
     return end_date.toISOString().slice(0, -5) + "+0000";
 }
 
-const FORM_STATE_INIT_VALUE: Inputs = {importYear: (new Date().getFullYear() - 1).toString(), importRange: "", toCalendar: "", username: "", password: "", ignoreOtherEvents: true} as Inputs;
+const FORM_STATE_INIT_VALUE: Inputs = {
+    importYear: (new Date().getFullYear() - 1).toString(),
+    importRange: "",
+    toCalendar: "",
+    username: "",
+    password: "",
+    ignoreOtherEvents: true,
+} as Inputs;
+
+type Range = {start: Date; end: Date};
+function get_quarter_ranges(year: number) {
+    return {
+        _1q_start: new Date(year, 4 - 1, 1),
+        _2q_start: new Date(year, 6 - 1, 10),
+        _3q_start: new Date(year, 9 - 1, 1),
+        _4q_start: new Date(year, 11 - 1, 25),
+
+        _1q_end: new Date(year, 6 - 1, 9, 23, 59, 59),
+        _2q_end: new Date(year, 8 - 1, 31, 23, 59, 59),
+        _3q_end: new Date(year, 11 - 1, 24, 23, 59, 59),
+        _4q_end: new Date(year + 1, 2 - 1, new Date(year, 2, 0).getDate(), 23, 59, 59),
+    };
+}
 
 const INIT_REQUIRE_VALUE_LIST = ["importRange", "toCalendar", "username", "password"];
 
@@ -103,10 +127,9 @@ export default function ImportForm() {
             return;
         }
         setAppState("import");
-        let class_events = data.events;
+        let class_events: Array<ClassEvent> = data.events;
         if (formState.ignoreOtherEvents) class_events = data.events.filter((e: ClassEvent) => e.className.indexOf("eventJugyo") !== -1);
         setImportCount(0);
-        setTotalImportCount(class_events.length);
         await postToGoogleCalendar(class_events);
         setAppState("ready");
     };
@@ -114,16 +137,21 @@ export default function ImportForm() {
     const getEventList = async () => {
         setAppState("connect portal");
         let res;
-        let search_param_obj = {importYear: formState.importYear, importRange: formState.importRange, username: formState.username, password: formState.password};
-        let url_search_param_str = new URLSearchParams(search_param_obj).toString();
+        let query_param_obj = {
+            importYear: formState.importYear,
+            importRange: formState.importRange,
+            username: formState.username,
+            password: formState.password,
+        };
+        let query_param_str = new URLSearchParams(query_param_obj).toString();
         try {
-            res = await fetch(process.env.NEXT_PUBLIC_API_DOMAIN + "/get_dhu_event_list?" + url_search_param_str, {method: "GET"});
+            res = await fetch(process.env.NEXT_PUBLIC_API_DOMAIN + "/get_dhu_event_list?" + query_param_str, {method: "GET"});
+            res = await res.json();
         } catch {
             throw new Error("サーバーに接続できませんでした");
         }
-        const data = await res.json();
-        if (data.status_code == "401" && data.detail == "user id or password is invalid") throw new Error("ユーザー名またはパスワードが違います");
-        return data;
+        if (res.status_code == "401" && res.detail == "user id or password is invalid") throw new Error("ユーザー名またはパスワードが違います");
+        return res;
     };
 
     function existsStateEmpty() {
@@ -149,12 +177,46 @@ export default function ImportForm() {
         setDhuPortalInputError({username: username_error_msg, password: password_error_msg});
     }
 
-    const postToGoogleCalendar = async (class_events: Array<any>) => {
-        //if (!(session && session.user)) return;
-        //let google_api_url = `https://www.googleapis.com/calendar/v3/calendars/${formState.toCalendar}/events`;
-        //let res = await fetch(google_api_url, {method: "GET", headers: {Authorization: `Bearer ${session.accessToken}`, "Content-Type": "application/json"}});
+    const postToGoogleCalendar = async (class_events: Array<ClassEvent>) => {
+        if (!(session && session.user)) return;
+        let res_obj: CalendarList;
+        let next_page_token: string = "";
+        let already_posted_event_list: Array<Event> = [];
+        do {
+            let {start, end} = get_quarter_range(+formState.importYear, formState.importRange);
+            let query_param_obj;
+            if (next_page_token != "")
+                query_param_obj = {
+                    pageToken: next_page_token,
+                };
+            else
+                query_param_obj = {
+                    maxResults: 2000,
+                    timeMax: end.toISOString(),
+                    timeMin: start.toISOString(),
+                    orderBy: "startTime",
+                    singleEvents: true,
+                };
+            console.log(start.toISOString(), end.toISOString());
+            let query_param_str = new URLSearchParams(query_param_obj).toString();
+            let google_api_url = `https://www.googleapis.com/calendar/v3/calendars/${formState.toCalendar}/events?${query_param_str}`;
+            let res = await fetch(google_api_url, {
+                method: "GET",
+                headers: {Authorization: `Bearer ${session.accessToken}`, "Content-Type": "application/json"},
+            });
+            res_obj = await res.json();
+            console.log(res_obj);
+            if (res_obj.hasOwnProperty("error")) throw Error(`status ${res_obj.status}`);
+            if (res_obj.nextPageToken) next_page_token = res_obj.nextPageToken;
+            already_posted_event_list.push(...res_obj.items);
+        } while (res_obj.hasOwnProperty("nextPageToken"));
+        console.log(res_obj.items);
         // TODO: res.statusが400と500のときのエラー処理
-
+        class_events = class_events.filter((class_event, i) => !is_event_duplicated(already_posted_event_list, class_event));
+        let {start, end} = get_quarter_range(parseInt(formState.importYear), formState.importRange);
+        // importRange外の授業を削除する
+        class_events = class_events.filter((class_event, i) => new Date(class_event.start).getTime() > start.getTime() && new Date(class_event.start).getTime() < end.getTime());
+        setTotalImportCount(class_events.length);
         for (const class_event of class_events) {
             try {
                 await addEventToGoogleCal(class_event.start, class_event.title);
@@ -164,6 +226,16 @@ export default function ImportForm() {
             }
         }
     };
+
+    function get_quarter_range(year: number, importRange: string): Range {
+        let r = get_quarter_ranges(year);
+        if (importRange == "1q") return {start: r._1q_start, end: r._1q_end};
+        else if (importRange == "2q") return {start: r._2q_start, end: r._2q_end};
+        else if (importRange == "3q") return {start: r._3q_start, end: r._3q_end};
+        else if (importRange == "4q") return {start: r._4q_start, end: r._4q_end};
+        else if (importRange == "1q_and_2q") return {start: r._1q_start, end: r._2q_end};
+        return {start: r._3q_start, end: r._4q_end};
+    }
 
     async function addEventToGoogleCal(start: string, title: string) {
         if (!(session && session.user)) return;
@@ -185,6 +257,17 @@ export default function ImportForm() {
             throw new Error(`${res.status} : Bad response from server`);
         }
         setImportCount((prevCount) => prevCount + 1);
+    }
+
+    //fkjdskf
+
+    // If we sort Event, we can bisect the search by date.
+    function is_event_duplicated(already_posted_event_list: Event[], class_event: ClassEvent): boolean {
+        for (const already_posted_event of already_posted_event_list) {
+            if (!already_posted_event.start.dateTime) continue;
+            if (class_event.title == already_posted_event.summary && new Date(class_event.start).toISOString() == new Date(already_posted_event.start.dateTime).toISOString()) return true;
+        }
+        return false;
     }
 
     return (

c403b2315c3fd1b817e4ef14fc5c7f157142fb8c
cou <courange.c@gmail.com>
2023-02-19 21:21:06 +0900

    try add ogp

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,8 +1,10 @@
 import ImportForm from "../components/ImportForm";
 import {Container} from "@mui/material";
+import _Head from "../components/Head";
 export default function Home() {
     return (
         <>
+            <_Head />
             <Container maxWidth="sm">
                 <ImportForm></ImportForm>
             </Container>

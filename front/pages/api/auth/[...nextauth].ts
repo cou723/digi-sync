f8ca5015af215ca3876c4d7e1191a875e0cf74b6
cou <courange.c@gmail.com>
2023-06-18 00:08:53 +0900

    add: google認証関連にデバッグ用の出力追加

--- front/pages/api/auth/[...nextauth].ts
+++ front/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,19 @@
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 console.log(process.env.NEXT_PUBLIC_GOOGLE_ID || '', process.env.NEXT_PUBLIC_GOOGLE_SECRET)
+
+const GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_ID || '';
+const GOOGLE_CLIENT_SECRET = process.env.NEXT_PUBLIC_GOOGLE_SECRET || '';
+
+if(GOOGLE_CLIENT_ID == '' || GOOGLE_CLIENT_SECRET == '') {
+    throw new Error('Google OAuth2.0のクライアントIDまたはクライアントシークレットが設定されていません')
+}
+
 export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.NEXT_PUBLIC_GOOGLE_ID || '',
-            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET || '',
+            clientId: GOOGLE_CLIENT_ID,
+            clientSecret: GOOGLE_CLIENT_SECRET,
             authorization: { params: { scope: 'openid https://www.googleapis.com/auth/calendar' } },
             httpOptions: {
                 timeout: 60000,

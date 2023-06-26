d435dd9c8bdb87bf938d2bfcc6a8ec8eb1506459
cou <courange.c@gmail.com>
2023-06-26 15:21:48 +0900

    feat: Fixed #79

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -8,17 +8,25 @@ import ImportForm from "../components/ImportForm";
 
 export default function Home() {
     const { t } = useTranslation("pages");
+    const { t: ct } = useTranslation("common");
     const { status: authStatus } = useSession();
     return (
         <>
             <HEAD />
             <Container sx={{ pt: 2 }} maxWidth='sm'>
                 {authStatus == "unauthenticated" && (
-                    <Alert severity='error'>
+                    <Alert sx={{ mt: 2 }} severity='error'>
                         {t("index.error")}
                         <Link href='/ical'>{t("index.here")}</Link>
                     </Alert>
                 )}
+                <Alert sx={{ mt: 2 }} severity='info'>
+                    このプロジェクトでは現在翻訳者を募集中です。
+                    もし留学生の方で母国語に翻訳してくださる方がいたら
+                    <Link href='https://twitter.com/coffeec91065940'>開発者のTwitter</Link>
+                    までご連絡ください。
+                </Alert>
+
                 <ImportForm />
             </Container>
         </>
